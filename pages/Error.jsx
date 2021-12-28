fa01c55e534b98f3642de42c171e89cdcb998011
jdpena28 <johnhenrich1128@gmail.com>
2021-12-28 14:25:56 +0800

    CHORE: add verification error page

---
+++ pages/Error.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import Layout from '../src/components/Layout'
+import StepIndicator from '../src/components/StepIndicator'
+import Button from '../src/components/Button'
+const Error = () => {
+    return (
+        <Layout>
+      <Head>
+        <title>Verification Failed</title>
+      </Head>
+      <StepIndicator stepColor2={"bg-highlight"} stepColor3={"bg-highlight"} />
+      <div className=' w-max h-max  absolute text-center inset-0 my-auto mx-auto'>
+        <Image src={"/failed.png"} height={150} width={150} />
+        <h3 className='text-3xl font-bold'>Verification Failed</h3>
+        <p className='text-lg'>
+          Verification Error, <br /> You can go back and try again to verify your identity.
+        </p>
+      </div>
+      <Button btnType={'button'} btnText={'Verify Again'} link={"/"}/>
+    </Layout>
+    )
+}
+
+export default Error
