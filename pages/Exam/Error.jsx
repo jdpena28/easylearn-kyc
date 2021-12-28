c1ddc9b5a64b4e8d97b5be3785100ef83d990e72
jdpena28 <johnhenrich1128@gmail.com>
2021-12-28 18:23:09 +0800

    CHORE: add error pages for pre-exam

---
+++ pages/Exam/Error.jsx
@@ -0,0 +1,20 @@
+import Layout from "../../src/components/Layout"
+import Image from "next/image"
+import Button from "../../src/components/Button"
+
+const Error = () => {
+    return (
+        <Layout title={"Verification Failed"}>
+      <div className=' w-max h-max  absolute text-center inset-0 my-auto mx-auto'>
+        <Image src={"/failed.png"} alt='red error x mark' height={150} width={150} />
+        <h3 className='text-3xl font-bold'>Verification Failed</h3>
+        <p className='text-lg'>
+          Verification Error, <br /> You can go back and try again to verify your identity.
+        </p>
+      </div>
+      <Button btnType={'button'} btnText={'Verify Again'} link={"/Exam/Pre-Exam"}/>
+    </Layout>
+    )
+}
+
+export default Error
