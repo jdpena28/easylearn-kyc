c7be4afc91720813705a20854714ea873234ca9a
jdpena28 <johnhenrich1128@gmail.com>
2021-12-28 14:11:01 +0800

    FIX: Change div to button tag

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,10 +5,10 @@ import Link from 'next/link'
 const Button = () => {
     return (
         <Link href={'/Step2'} className='cursor-pointer'> 
-            <div className='bg-tertiary text-white w-52 h-16 rounded-md cursor-pointer flex justify-evenly absolute bottom-4 right-5'>
+            <button className='bg-tertiary text-white w-52 h-16 rounded-md cursor-pointer flex justify-evenly items-center absolute bottom-4 right-5'>
                 <p className='text-center text-xl font-semibold leading-[4rem]'>NEXT</p>
-                <Image className='fill-white'   src={'/icons/arrow-right-thin.svg'} height={10} width={30}/>
-            </div>
+                <Image className='fill-white'   src={'/icons/arrow-right-thin.svg'} height={30} width={30}/>
+            </button>
         </Link>
     )
 }
