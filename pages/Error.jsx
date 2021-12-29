34327020075eacee64134e37a2c5c371b6df053a
jdpena28 <johnhenrich1128@gmail.com>
2021-12-29 10:45:43 +0800

    CHORE: responsive design for error page

--- pages/Error.jsx
+++ pages/Error.jsx
@@ -6,14 +6,21 @@ const Error = () => {
     return (
       <Layout title={"Verification Failed"}>
       <StepIndicator stepColor2={"bg-highlight"} stepColor3={"bg-highlight"} />
-      <div className=' w-max h-max  absolute text-center inset-0 my-auto mx-auto'>
+      <div className=' w-max sm:w-full h-max  absolute text-center inset-0 my-auto mx-auto'>
         <Image src={"/failed.png"} alt='red error x mark' height={150} width={150} />
         <h3 className='text-3xl font-bold'>Verification Failed</h3>
         <p className='text-lg'>
           Verification Error, <br /> You can go back and try again to verify your identity.
         </p>
       </div>
-      <Button btnType={'button'} btnText={'Verify Again'} link={"/"}/>
+      <Button
+        btnType={"button"}
+        btnText={"Verify Again"}
+        classname={"sm:absolute  bottom-3 right-4"}
+        link={
+          "/"
+        }
+      />
     </Layout>
     )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ const Button = ({btnText,btnType,link,classname}) => {
         <Link href={link}> 
         <div className={`sm:flex  justify-end ${classname}`}>
             <button className='bg-tertiary text-white w-52 sm:w-40 h-16 sm:h-14 rounded-md flex justify-evenly items-center absolute sm:static bottom-4 right-5' type={btnType}>
-                <p className='text-center text-xl font-semibold leading-[4rem]'>{btnText}</p>
+                <p className='text-center text-xl sm:text-lg font-semibold leading-[4rem]'>{btnText}</p>
                 <Image className='fill-white'   src={'/icons/arrow-right-thin.svg'} height={30} width={30}/>
             </button>
         </div>
