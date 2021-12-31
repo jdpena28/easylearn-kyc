ca814bc559783da38cd1eb598bf2d3bdad6a07e6
jdpena28 <johnhenrich1128@gmail.com>
2021-12-31 10:39:28 +0800

    CHORE: add responsive design for pre-exam page

--- pages/Exam/Pre-Exam.jsx
+++ pages/Exam/Pre-Exam.jsx
@@ -1,10 +1,10 @@
-import { useRef, useState,useCallback } from "react"
+import { useRef, useState, useCallback } from "react"
 import Layout from "../../src/components/Layout"
 import Input from "../../src/components/Input"
 import Button from "../../src/components/Button"
 import Webcam from "react-webcam"
 
-import useUpdateEffect from '../../src/hooks/useUpdateEffect'
+import useUpdateEffect from "../../src/hooks/useUpdateEffect"
 
 const App = () => {
   const [enrollee, setEnrollee] = useState({
@@ -13,40 +13,35 @@ const App = () => {
     MiddleName: "",
   })
 
- 
-  
   const webcamRef = useRef(null)
   const [openCamera, setOpenCamera] = useState(false)
-  const [showBtnCapture,setShowBtnCapture] = useState(false)
-  const [counter,setCounter] = useState(3)
-  const [imgsrc,setImgsrc] = useState(null)
+  const [showBtnCapture, setShowBtnCapture] = useState(false)
+  const [counter, setCounter] = useState(3)
+  const [imgsrc, setImgsrc] = useState(null)
 
   const captureImg = () => {
-    if (counter==0){
+    if (counter == 0) {
       setCounter(3)
     } else {
-      setCounter(counter-1)
+      setCounter(counter - 1)
     }
   }
   useUpdateEffect(() => {
     counter > 0 && setTimeout(() => setCounter(counter - 1), 1000)
     counter == 0 && screenshot()
-  } ,[counter])
+  }, [counter])
 
-  const screenshot = useCallback(
-    () => {
-      setImgsrc(webcamRef.current.getScreenshot())
-    },
-    [webcamRef]
-  );
+  const screenshot = useCallback(() => {
+    setImgsrc(webcamRef.current.getScreenshot())
+  }, [webcamRef])
 
   return (
     <Layout title={"Pre-exam Identification"}>
       <div className='container mx-auto'>
         <h3 className='font-bold text-2xl'>Pre-exam Identification</h3>
-        <div className='mx-auto max-w-6xl'>
+        <div className='mx-auto max-w-6xl sm:max-w-full'>
           <form className='space-y-5'>
-            <div className='flex justify-between mt-3'>
+            <div className='flex sm:block justify-between mt-3 sm:space-y-2'>
               <Input
                 label='Last Name'
                 placeholder='Doe'
@@ -70,20 +65,53 @@ const App = () => {
               />
             </div>
 
-            <div className='grid grid-cols-5 justify-end'>
+            <div className='grid sm:block grid-cols-5 justify-end'>
               <div className='space-y-3'>
                 <div className='flex w-full'></div>
                 <p className='text-2xl font-bold'>
                   Lets take a picture of you.
                 </p>
-                <p className='text-sm w-64 leading-none'>
+                <p className='text-sm w-64 sm:w-full leading-none'>
                   *This will be used for identification purposes. The picture
                   will cross match from the pre enrollment ID you provided.
                 </p>
-                <p className="w-44 h-12 overflow-scroll whitespace-pre-line">Image:&nbsp;{imgsrc}</p>
+                <p className='w-44 sm:w-full h-12 truncate'>
+                  Image:&nbsp;{imgsrc}
+                </p>
+                <div className='relative mx-auto w-max sm:w-full hidden sm:flex'>
+                  <Webcam
+                    audio={false}
+                    mirrored={true}
+                    className='mx-auto sm:w-full'
+                    screenshotFormat='image/jpeg'
+                    ref={webcamRef}
+                    width={480}
+                    onUserMedia={() => {
+                      setShowBtnCapture(true)
+                    }}
+                  />
+                  {counter > 0 && (
+                    <div className='h-full w-full absolute flex justify-center items-center'>
+                      <p className='font-extrabold text-white text-6xl'>
+                        {counter}
+                      </p>
+                    </div>
+                  )}
+                </div>
+                {showBtnCapture && (
+                  <button
+                  type='button'
+                    onClick={captureImg}
+                    className='bg-slate-700 p-3 text-white w-32 hidden sm:block'
+                  >
+                    Capture
+                  </button>
+                )}
+
+                {/* WEBCAM CAMERA FUNCTION FOR DESKTOP VIEW */}
                 <button
                   type='button'
-                  className='font-semibold bg-tertiary text-white p-2'
+                  className='sm:hidden font-semibold bg-tertiary text-white p-2'
                   onClick={() =>
                     openCamera ? setOpenCamera(false) : setOpenCamera(true)
                   }
@@ -91,30 +119,48 @@ const App = () => {
                   {openCamera ? "Close" : "Open"} Camera
                 </button>
               </div>
-              <div className='col-span-3 col-start-3'>
+              <div className='sm:hidden col-span-3 col-start-3'>
                 <h4 className='font-semibold text-2xl'>CAMERA</h4>
                 <div className='h-max w-max space-y-2  bg-gray-200'>
                   {openCamera && (
-                   <div className='relative mx-auto w-max flex'>
-                   <Webcam
-                     audio={false}
-                     mirrored={true}
-                     className='mx-auto'
-                     screenshotFormat='image/jpeg'
-                     ref={webcamRef}
-                     width={450}
-                     onUserMedia={()=>{setShowBtnCapture(true)}}
-                   />
-                  {(counter>0) && <div className="h-full w-full absolute flex justify-center items-center">
-                     <p className="font-extrabold text-white text-6xl">{counter}</p>
-                   </div>}
-                 </div>
+                    <div className='relative mx-auto w-max flex'>
+                      <Webcam
+                        audio={false}
+                        mirrored={true}
+                        className='mx-auto'
+                        screenshotFormat='image/jpeg'
+                        ref={webcamRef}
+                        width={450}
+                        onUserMedia={() => {
+                          setShowBtnCapture(true)
+                        }}
+                      />
+                      {counter > 0 && (
+                        <div className='h-full w-full absolute flex justify-center items-center'>
+                          <p className='font-extrabold text-white text-6xl'>
+                            {counter}
+                          </p>
+                        </div>
+                      )}
+                    </div>
                   )}
-                  {(showBtnCapture&&openCamera) && <button type="button" onClick={captureImg} className="bg-slate-700 p-3 text-white w-32">Capture</button>}
+                  {showBtnCapture && openCamera && (
+                    <button
+                      type='button'
+                      onClick={captureImg}
+                      className='bg-slate-700 p-3 text-white w-32'
+                    >
+                      Capture
+                    </button>
+                  )}
                 </div>
               </div>
             </div>
-            <Button link={"/Exam/ExamCoupon"} btnText='NEXT' btnType={"submit"} />
+            <Button
+              link={"/Exam/ExamCoupon"}
+              btnText='NEXT'
+              btnType={"submit"}
+            />
           </form>
         </div>
       </div>
