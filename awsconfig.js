d4a040e57678d0f0520eb0c38928e16f40b4b65a
jdpena28 <johnhenrich1128@gmail.com>
2022-01-06 22:10:14 +0800

    use aws access key hopefully fix

--- awsconfig.js
+++ awsconfig.js
@@ -1,10 +1,11 @@
 import {RekognitionClient} from '@aws-sdk/client-rekognition'
 
+
 const rekognitionClient =  new RekognitionClient({
     region: 'ap-southeast-1',
     credentials: {
-        accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY,
-        secretAccessKey: process.env.NEXT_PUBLIC_SECRET_KEY,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     }
 })
 export default rekognitionClient
