e06be7ed6dd2605481264b299ae988bedbc40112
jdpena28 <johnhenrich1128@gmail.com>
2022-01-06 11:31:03 +0800

    Use environment variable for api keys

--- awsconfig.js
+++ awsconfig.js
@@ -3,8 +3,8 @@ import {RekognitionClient} from '@aws-sdk/client-rekognition'
 const rekognitionClient =  new RekognitionClient({
     region: 'ap-southeast-1',
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.ACCESS_KEY,
+        secretAccessKey: process.env.SECRET_KEY,
     }
 })
 export default rekognitionClient
--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    ACCESS_KEY: process.env.ACCESS_KEY,
+    SECRET_KEY: process.env.SECRET_KEY,
+  }
 }
