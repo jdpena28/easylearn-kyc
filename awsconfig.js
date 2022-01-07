f8fc0a3c23b1c4a1bb285aa2eef3de37072504df
jdpena28 <johnhenrich1128@gmail.com>
2022-01-07 11:29:39 +0800

    hotfixing: use next_public instead

--- awsconfig.js
+++ awsconfig.js
@@ -3,8 +3,8 @@ import {RekognitionClient} from '@aws-sdk/client-rekognition'
 const rekognitionClient =  new RekognitionClient({
     region: 'ap-southeast-1',
     credentials: {
-        accessKeyId: process.env.ACCESS_KEY_ID,
-        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+        accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY_ID,
+        secretAccessKey: process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY,
     }
 })
 export default rekognitionClient
--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-require("dotenv").config({ path: "./.env.custom" });
-
 module.exports = {
   reactStrictMode: true,
   env : {
-    ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
-    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
+    ACCESS_KEY_ID: process.env.NEXT_PUBLIC_ACCESS_KEY_ID,
+    SECRET_ACCESS_KEY: process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY,
   }
 }
