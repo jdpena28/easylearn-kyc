876e7c34903fa0d714fa8a1607df04134a90b6e3
jdpena28 <johnhenrich1128@gmail.com>
2022-01-07 11:20:20 +0800

    BUGFIX: use shorthand syntax

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ require("dotenv").config({ path: "./.env.custom" });
 module.exports = {
   reactStrictMode: true,
   env : {
-    ACCESS_KEY_ID = process.env.ACCESS_KEY_ID,
-    SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY,
+    ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
+    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
   }
 }
